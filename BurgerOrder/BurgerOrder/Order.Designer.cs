9fca7bfc4f97dcb90eac10c0e696eeb16eb809d4
thieril <thieril@dunwoody.edu>
2016-01-29 11:16:03 -0600

    Burger Order Initial Code 012816
    Burger Order Initial Code 012816

---
+++ BurgerOrder/BurgerOrder/Order.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Order
+{
+    partial class Order
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lbl1Output = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(172, 75);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lbl1Output
+            // 
+            this.lbl1Output.AutoSize = true;
+            this.lbl1Output.Location = new System.Drawing.Point(79, 84);
+            this.lbl1Output.Name = "lbl1Output";
+            this.lbl1Output.Size = new System.Drawing.Size(71, 13);
+            this.lbl1Output.TabIndex = 1;
+            this.lbl1Output.Text = "Confirm Order";
+            this.lbl1Output.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Order
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl1Output);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Order";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Order_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lbl1Output;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BurgerOrder/BurgerOrder/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Order
+{
+    public partial class Order : Form
+    {
+        string message = "Button Clicked!";
+
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lbl1Output.Text = ""; //Button.btnSubmit;
+        }
+
+        private void Order_Load(object sender, EventArgs e)
+        {
+            lbl1Output.Text = message;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //lblOne.Text = "Changed";
+        }
+    }
+}
